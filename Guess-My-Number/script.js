8458f99020f96fcf725f10094a425e4f99332645
tiagoassunc <tiagoassunc@gmail.com>
2021-03-08 09:06:32 -0300

    "Handling Click Events"

--- Guess-My-Number/script.js
+++ Guess-My-Number/script.js
@@ -1,10 +1,19 @@
 'use strict';
 
-console.log(document.querySelector('.message').textContent);
+/* console.log(document.querySelector('.message').textContent);
 document.querySelector('.message').textContent = '🥳 Correct Number!';
 
 document.querySelector('.number').textContent = 13;
 document.querySelector('.score').textContent = 10;
 
 document.querySelector('.guess').value = 23;
-console.log(document.querySelector('.guess').value);
+console.log(document.querySelector('.guess').value); */
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔ No number!';
+  }
+});
