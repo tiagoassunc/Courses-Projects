9fdcfd197206eeb0b33347fcdf0ac3805fdcbf51
tiagoassunc <tiagoassunc@gmail.com>
2021-03-09 20:40:56 -0300

    "Correção de Bug"

--- Pig-Game/script.js
+++ Pig-Game/script.js
@@ -79,7 +79,7 @@ btnHold.addEventListener('click', function () {
     document.getElementById(`score--${activePlayer}`).textContent =
       scores[activePlayer];
     // 2. Check if player's' score is >= 100
-    if (scores[activePlayer] >= 100) {
+    if (scores[activePlayer] >= 20) {
       // Finish the game
       playing = false;
       diceEl.classList.add('hidden');
@@ -94,4 +94,6 @@ btnHold.addEventListener('click', function () {
   }
 });
 
-btnNew.addEventListener('click', init());
+btnNew.addEventListener('click', function () {
+  init;
+});
