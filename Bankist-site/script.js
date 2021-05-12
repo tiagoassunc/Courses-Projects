70ccb63fab2c0bd01dc260a494a29ddd31f56ef4
Tiago Assunção <tiagoassunc@gmail.com>
2021-05-12 18:46:34 -0300

    "Implementing Smooth Scrolling"

--- Bankist-site/script.js
+++ Bankist-site/script.js
@@ -32,9 +32,42 @@ document.addEventListener('keydown', function (e) {
   }
 });
 
+// Implementing Smooth Scrolling
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click', function (e) {
+  const s1coords = section1.getBoundingClientRect(); // Getting coordinates of section1
+  console.log(s1coords);
+
+  console.log(e.target.getBoundingClientRect()); // e.target element with event => btnScrollTo
+
+  console.log('Current scroll (X/Y)', window.pageXOffset, pageYOffset); // Scroll coordinates
+
+  console.log(
+    'height/width viewport',
+    document.documentElement.clientHeight,
+    document.documentElement.clientWidth
+  );
+
+  // Scrolling
+  // window.scrollTo(
+  //   s1coords.left + window.pageXOffset,
+  //   s1coords.top + window.pageYOffset
+  // );
+
+  // window.scrollTo({
+  //   left: s1coords.left + window.pageXOffset,
+  //   top: s1coords.top + window.pageYOffset,
+  //   behavior: 'smooth',
+  // });
+
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
+
 // Lectures /////////////////////////////////////////////////////////////////////
 
-///// Selecting Elements /////
+/* ///// Selecting Elements /////
 console.log(document.documentElement);
 console.log(document.head);
 console.log(document.body);
@@ -115,4 +148,4 @@ logo.classList.toggle('c');
 logo.classList.contains('c');
 
 // Don't use
-logo.className = 'Tiago';
+logo.className = 'Tiago'; */
