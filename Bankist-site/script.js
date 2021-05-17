bc1681907cca810136e3d71f52d334b217201644
Tiago Assunção <tiagoassunc@gmail.com>
2021-05-17 10:26:06 -0300

    "DOM Traversing"

--- Bankist-site/script.js
+++ Bankist-site/script.js
@@ -227,3 +227,33 @@ document.querySelector('.nav').addEventListener(
   },
   true // Listing event travel down the dom NOT bubbling
 ); */
+
+//// DOM Traversing  ////
+const h1 = document.querySelector('h1');
+
+// Going downwards: child
+console.log(h1.querySelectorAll('.highlight')); // Children of h1 with highlight class
+console.log(h1.childNodes); // Every single node and every single type
+console.log(h1.children); // Elements inside h1 => direct children
+console.log(h1.firstElementChild);
+h1.firstElementChild.style.color = 'white'; // Styling just first children
+h1.lastElementChild.style.color = 'blueviolet'; // Styling just last children
+
+// Going upwards: parents
+console.log(h1.parentNode);
+console.log(h1.parentElement);
+h1.closest('.header').style.background = 'var(--gradient-secondary)'; // Closest parent => in this case closest header
+
+h1.closest('h1').style.background = 'var(--gradient-primary)'; // Itself
+
+// Going sideways: siblings
+console.log(h1.previousElementSibling); // Just previus and next one
+console.log(h1.nextElementSibling); // Just previus and next one
+
+console.log(h1.previousSibling);
+console.log(h1.nextSibling);
+
+console.log(h1.parentElement.children); // How acess all siblings
+[...h1.parentElement.children].forEach(function (el) {
+  if (el !== h1) el.style.transform = 'scale(0.5)';
+});
