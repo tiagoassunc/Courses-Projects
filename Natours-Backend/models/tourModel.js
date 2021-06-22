656e1d405f0cd2548f2b599d09f7f5894847abad
Tiago Assunção <tiagoassunc@gmail.com>
2021-06-22 13:47:30 -0300

    "Modelling the Tours"

--- Natours-Backend/models/tourModel.js
+++ Natours-Backend/models/tourModel.js
@@ -1,14 +1,30 @@
 const mongoose = require('mongoose');
 
-//// Creating a Simple Tour Model ////
-// Schema => blueprint
+//// Modelling the Tours //// Schema => blueprint
 const tourSchema = new mongoose.Schema({
   name: {
     type: String,
     required: [true, 'A tour must have a name'],
     unique: true,
+    trim: true,
   },
-  rating: {
+  duration: {
+    type: Number,
+    required: [true, 'A tour must have a duration'],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A tour must have a group size'],
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'A tour must have a difficulty'],
+  },
+  ratingsAverage: {
+    type: Number,
+    default: 4.5,
+  },
+  ratingsQuantity: {
     type: Number,
     default: 4.5,
   },
@@ -16,6 +32,26 @@ const tourSchema = new mongoose.Schema({
     type: Number,
     required: [true, 'A tour must have a price'],
   },
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    trim: true,
+    required: [true, 'A tour must have a summary'],
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'A tour must have a cover image'],
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  startDates: [Date],
 });
 // Model of schema
 const Tour = mongoose.model('Tour', tourSchema);
